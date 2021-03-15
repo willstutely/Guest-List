4530829e744092184f416edec6209b14653f0d5d
willstutely <jonathanandrewdrake@gmail.com>
2021-03-15 17:55:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -26,9 +26,11 @@ function supportsLocalStorage() {
   }
 }
 
+
 // Conditional checks if localStorage is possible, and if true then the rest of the code is applicable. This concept 
 // was shown in the TreeHouse Local Storage Workshop.
 if (supportsLocalStorage()) {
+
 
   // getInvitedList, saveInvitedList, and clearInvitedList functions written during the TreeHouse Local Storage Workshop.
   // I just changed the names to make them relevant for this program.
@@ -56,145 +58,149 @@ if (supportsLocalStorage()) {
     localStorage.clear();
   }
 
-  const alreadyInvited = getInvitedList();
-      for (let i = 0; i>alreadyInvited.length; i++) {
-         createLi(alreadyInvited[i]);
-      }
-
   document.addEventListener('DOMContentLoaded', () => {
     
-        const form = document.getElementById('registrar');
-        const input = form.querySelector('input');
-        
-        const mainDiv = document.querySelector('.main');
-        const ul = document.getElementById('invitedList');
-        
-        const div = document.createElement('div');
-        const filterLabel = document.createElement('label');
-        const filterCheckbox = document.createElement('input');
+    const form = document.getElementById('registrar');
+    const input = form.querySelector('input');
     
-        // create checkbox that filters out the confirmed guests
-        filterLabel.textContent = "Hide those who haven't responded";
-        filterCheckbox.type = 'checkbox';
-        div.appendChild(filterLabel);
-        div.appendChild(filterCheckbox);
-        mainDiv.insertBefore(div, ul);
-        filterCheckbox.addEventListener('change', (e) => {
-          const isChecked = e.target.checked;
-          const lis = ul.children;
-          if (isChecked) {
-            for (let i = 0; i < lis.length; i += 1 ) {
-              let li = lis[i];
-              if (li.className === 'responded') {
-                  li.style.display = '';
-            } else {
-                li.style.display = 'none'; 
+    const mainDiv = document.querySelector('.main');
+    const ul = document.getElementById('invitedList');
+    
+    const div = document.createElement('div');
+    const filterLabel = document.createElement('label');
+    const filterCheckbox = document.createElement('input');
+
+    window.onload = function() {
+      const alreadyInvited = getInvitedList();
+      alreadyInvited.forEach(function(i) {
+        const li = createLi(i);
+        ul.appendChild(li);
+      })
+      
+    }
+    // create checkbox that filters out the confirmed guests
+    filterLabel.textContent = "Hide those who haven't responded";
+    filterCheckbox.type = 'checkbox';
+    div.appendChild(filterLabel);
+    div.appendChild(filterCheckbox);
+    mainDiv.insertBefore(div, ul);
+    filterCheckbox.addEventListener('change', (e) => {
+      const isChecked = e.target.checked;
+      const lis = ul.children;
+      if (isChecked) {
+        for (let i = 0; i < lis.length; i += 1 ) {
+          let li = lis[i];
+          if (li.className === 'responded') {
+              li.style.display = '';
+        } else {
+            li.style.display = 'none'; 
+        }
             }
-                }
-            } else {
-            for (let i = 0; i <lis.length; i += 1) {
-                let li = lis[i];
-                  li.style.display = '';
-                }
-          }
-        });
+        } else {
+        for (let i = 0; i <lis.length; i += 1) {
+            let li = lis[i];
+              li.style.display = '';
+            }
+      }
+    });
+
+    // the createLi function takes the input from the user and uses it to create a guest card
+    function createLi(text) {
+        
+      function createElement(elementName, property, value) {
+        const element = document.createElement(elementName);
+        element[property] = value;
+        return element;
+      }
+      function appendToLi(elementName, property, value) {
+        const element = createElement(elementName, property, value);
+        li.appendChild(element);
+        return element;
+      }
+      const li = document.createElement('li');
+      appendToLi('span', 'textContent', text);
+      appendToLi('label', 'textContent', 'Confirmed')
+      .appendChild(createElement('input', 'type', 'checkbox'));
+      appendToLi('button', 'textContent', 'edit');
+      appendToLi('button', 'textContent', 'remove');
+      // populate the array created earlier of just the names entered to be able to compare and check for duplicates
+      liArray.push(text);
+      return li;
+    }
+  
     
-        // the createLi function takes the input from the user and uses it to create a guest card
-        function createLi(text) {
-          
-          function createElement(elementName, property, value) {
-            const element = document.createElement(elementName);
-            element[property] = value;
-            return element;
-          }
-          function appendToLi(elementName, property, value) {
-            const element = createElement(elementName, property, value);
-            li.appendChild(element);
-            return element;
-          }
-          const li = document.createElement('li');
-          appendToLi('span', 'textContent', text);
-          appendToLi('label', 'textContent', 'Confirmed')
-          .appendChild(createElement('input', 'type', 'checkbox'));
-          appendToLi('button', 'textContent', 'edit');
-          appendToLi('button', 'textContent', 'remove');
-          // populate the array created earlier of just the names entered to be able to compare and check for duplicates
-          liArray.push(text);
-          return li;
-        }
-        
-        
-        function checkForDuplicates(arr) {
-          for (let i = 0; i < arr.length; i += 1) {
-            
-            return arr[i];
-          }
-        }
     
-        form.addEventListener('submit', (e) => {
-          e.preventDefault();
-          const text = input.value;
-          input.value = "";
-          if (text.length == 0) {
-            alert('Please Enter a Name')
-            // conditional that compares the entered text to the returned value of the checkForDuplicates function. User
-            // is alerted if there is a match
-          } else if (text === checkForDuplicates(liArray)) {
-            alert('That name has already been invited')
-          } else {
-          const li = createLi(text);
-          ul.appendChild(li);
-          saveInvitedList(text)
-          }
-          
-        });  
+    function checkForDuplicates(arr) {
+      for (let i = 0; i < arr.length; i += 1) {
         
-        ul.addEventListener('change', (e) => {
-          const checkbox = e.target;
-          const checked = checkbox.checked;
-          const listItem = checkbox.parentNode.parentNode;
-          
-          if (checked) {
-            listItem.className = 'responded';
-          } else {
-            listItem.className = '';
+        return arr[i];
+      }
+    }
+
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const text = input.value;
+      input.value = "";
+      if (text.length == 0) {
+        alert('Please Enter a Name')
+        // conditional that compares the entered text to the returned value of the checkForDuplicates function. User
+        // is alerted if there is a match
+      } else if (text === checkForDuplicates(liArray)) {
+        alert('That name has already been invited')
+      } else {
+      const li = createLi(text);
+      ul.appendChild(li);
+      saveInvitedList(text)
+      }
+      
+    });  
+    
+    ul.addEventListener('change', (e) => {
+      const checkbox = e.target;
+      const checked = checkbox.checked;
+      const listItem = checkbox.parentNode.parentNode;
+      
+      if (checked) {
+        listItem.className = 'responded';
+      } else {
+        listItem.className = '';
+      }
+    });
+    
+    ul.addEventListener('click', (e) => {
+      if (e.target.tagName === 'BUTTON') {
+        const button = e.target;
+        const li = button.parentNode;
+        const ul = li.parentNode;
+        const action = button.textContent;
+        const nameActions = {
+          remove: () => {
+            ul.removeChild(li);
+        },
+          edit: () => {
+            const span = li.firstElementChild;
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = span.textContent;
+            li.insertBefore(input, span);
+            li.removeChild(span);
+            button.textContent = 'save';
+        },
+          save: () => {
+            const input = li.firstElementChild;
+            const span = document.createElement('span');
+            span.textContent = input.value;
+            li.insertBefore(span, input);
+            li.removeChild(input);
+            button.textContent = 'edit';
           }
-        });
+        };
         
-        ul.addEventListener('click', (e) => {
-          if (e.target.tagName === 'BUTTON') {
-            const button = e.target;
-            const li = button.parentNode;
-            const ul = li.parentNode;
-            const action = button.textContent;
-            const nameActions = {
-              remove: () => {
-                ul.removeChild(li);
-            },
-              edit: () => {
-                const span = li.firstElementChild;
-                const input = document.createElement('input');
-                input.type = 'text';
-                input.value = span.textContent;
-                li.insertBefore(input, span);
-                li.removeChild(span);
-                button.textContent = 'save';
-            },
-              save: () => {
-                const input = li.firstElementChild;
-                const span = document.createElement('span');
-                span.textContent = input.value;
-                li.insertBefore(span, input);
-                li.removeChild(input);
-                button.textContent = 'edit';
-              }
-            };
-            
-          // select and run action in button's name
-            nameActions[action]();
-            
-          }
-        });
+      // select and run action in button's name
+        nameActions[action]();
+        
+      }
+    });
 
   
   
